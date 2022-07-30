7ae9faf1f0e96adbe5953ffb138fef2936b8a4d4
brandon-banda <brandonvzx@gmail.com>
2022-07-30 04:40:39 -0500

    added filter

---
+++ devlog.md
@@ -0,0 +1,3 @@
+1. Implemented a filter to distinguish a W3A from a Notification instead of handling only the W3As that are named "DOC"
+
+2. wrote variables using pythons env variable for USERPROFILE for paths for neater code and ability to run on more than one machine
--- x64/file_renamer.py
+++ x64/file_renamer.py
@@ -2,41 +2,57 @@ from datetime import date
 import os
 
 date = date.today().strftime("%#m-%d-%Y")
+downloads = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Documents/Work/Downloads')
+main = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Documents/Work/')
 
-print(date) #log
+os.system('cls') # clear console
 
 ### Use Windows explorer Preview pane to quickly check pages
 
-os.chdir('C:/Users/admin/Documents/work/Downloads')
+### TODO Files need to move to the current date folder
+
+if not os.path.exists(downloads):
+    print('CREATING FILE...')
+    os.makedirs(downloads)
+
+os.chdir(downloads)
 print('Working in ' + os.getcwd() + "...")
 
-if not os.path.exists('C:/Users/admin/Documents/work/' + date + '/'):
+if not os.path.exists(main + date + '/'):
     print('CREATING FILE...')
-    os.makedirs('C:/Users/admin/Documents/work/' + date + '/')
+    os.makedirs(main + date + '/')
 
 
 W3A = "DOC"
+filt = ['DOC', 'Energy', 'Ltd', 'Company','Inc', 'Prod', 'Operat']
+a = "Noti"
 nid = 0
 wid = 0
 
 for file in os.listdir():
-    if W3A in file:
-        ftype = "W3A "
-        wid += 1
-        os.rename(file,'{}{} #{}.pdf'.format(ftype, date, wid))
-        print(file + ' -> ' '{}{} #{}'.format(ftype, date, wid)) #log
-        # if pages = 2, delete page 2
-        # if pages = 5, delete page 5
-        # if pages = 4, setType = 4pager
-            # COPY and delete pages 2, 3
-    else:
+    for thing in filt:
+        if thing in file:
+            ftype = "W3A "
+            wid += 1
+            os.rename(file,'{}{} #{}.pdf'.format(ftype, date, wid))
+            print(file + ' -> ' '{}{} #{}'.format(ftype, date, wid)) #log
+    if a in file:
         ftype = "DN "
         nid += 1
         os.rename(file,'{}{} #{}.pdf'.format(ftype, date, nid))
         print(file + ' -> ' '{}{} #{}'.format(ftype, date, nid)) #log
 
-os.chdir('C:/Users/admin/Documents/work/' + date + '/')
+#this stuff is for when we progamatically move files from the downloads to todays folder
+#os.chdir(main + date + '/')
+#print('Working in ' + os.getcwd() + "...") #log
 
-print('Working in ' + os.getcwd() + "...") #log
+print("------------Files Renamed---------")
 
-print("------------DONE---------")
+#now that we know which files are which (besides W3s), we can bring in pyLib to figure out how many pages they have
+# logic for this is :
+    #while page > 5 : delete page 5,  OR delete every page beyond number 5
+    #if page = 2 : delete page 2
+    #if page = 3 : PRINT UHHHHHHHHHH
+    #if page = 4 : delete pages 2, 3
+
+#at the end, purge the folder
